154ca330ed6f347890e4f98cc30a6781be83e8e7
madmoon <madmoontokyo@gmail.com>
2025-08-22 03:43:51 +0700

    x

---
+++ UniXcoder/downstream-tasks/code-performance/run.py
@@ -0,0 +1,59 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+import sys 
+import argparse
+import logging
+import os
+import pickle
+import random
+import torch
+import json
+import numpy as np
+from model import Model
+from torch.nn import CrossEntropyLoss, MSELoss
+from torch.utils.data import DataLoader, Dataset, SequentialSampler, RandomSampler,TensorDataset
+from torch.optim import AdamW
+from transformers import (WEIGHTS_NAME, get_linear_schedule_with_warmup,
+                              RobertaConfig, RobertaModel, RobertaTokenizer)
+
+logger = logging.getLogger(__name__)
+
+class InputFeatures(object):
+    """A single training/test features for a example."""
+    def __init__(self,
+                 code_tokens,
+                 code_ids,
+                 code_perf,
+    ):
+        self.code_tokens = code_tokens
+        self.code_ids = code_ids
+        self.code_perf = code_perf
+        
+def convert_examples_to_features(js, tokenizer, args):
+    """convert examples to token ids"""
+    code = ' '.join(js['code_tokens']) if type(js['code_tokens']) is list else ' '.join(js['code_tokens'].split())
+    code_tokens = tokenizer.tokenize(code)[:args.code_length-4]
+    code_tokens =[tokenizer.cls_token,"<encoder-only>",tokenizer.sep_token]+code_tokens+[tokenizer.sep_token]
+    code_ids = tokenizer.convert_tokens_to_ids(code_tokens)
+    padding_length = args.code_length - len(code_ids)
+    code_ids += [tokenizer.pad_token_id]*padding_length
+    code_perf = js['code_perf']
+    return InputFeatures(code_tokens, code_ids, code_perf)
+
+class TextDataset(Dataset):
+    def __init__(self, tokenizer, args, file_path=None):
+        self.examples = []
+        data = []
+        with open(file_path) as f:
+            if "jsonl" in file_path:
+                for line in f:
+                    line = line.strip()
+                    js = json.loads(line)
+                    data.append(js)
+        
+        for js in data:
+            self.examples.append(convert_examples_to_features(js, tokenizer, args))
+        
+        if 'train' in file_path:
+            for idx, example in enumerate(self.examples[:3]):
+                pass
